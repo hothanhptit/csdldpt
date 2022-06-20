ee561c6d56bbbd42ac2ff080cffe87be2dbd1aa8
thanhhd <thanhhd@aladintech.co>
2022-06-20 16:27:42 +0700

    first

---
+++ histogram.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import math
+
+# read image
+image = cv2.imread('./venv/Images/image1.jpg')
+# # calculate mean value from RGB channels and flatten to 1D array
+# vals = im.mean(axis=2).flatten()
+# # calculate histogram
+# counts, bins = np.histogram(vals, range(255))
+# # plot histogram centered on values 0..255
+# plt.bar(bins[:-1] - 0.5, counts, width=1, edgecolor='none')
+# plt.xlim([-0.5, 255.5])
+# # print (bin)
+# print (counts[0])
+# print (counts)
+# plt.show()
+
+colors = ("red", "green", "blue")
+channel_ids = (0, 1, 2)
+
+# create the histogram plot, with three lines, one for
+# each color
+plt.figure()
+plt.xlim([0, 256])
+for channel_id, c in zip(channel_ids, colors):
+    histogram, bin_edges = np.histogram(
+        image[:, :, channel_id], bins=256, range=(0, 256)
+    )
+    plt.plot(bin_edges[0:-1], histogram, color=c)
+    print (histogram)
+    # plt.xlim([-0.5, 255.5])
+
+plt.title("Color Histogram")
+plt.xlabel("Color value")
+plt.ylabel("Pixel count")
+
+
+plt.show()
---
+++ test.py
@@ -0,0 +1,4 @@
+import cv2 as cv2
+img = './venv/Images/image1.jpg'
+mask = cv2.imread(img,0)
+res = cv2.bitwise_and(img,img,mask = mask)
