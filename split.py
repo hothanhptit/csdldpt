11bf7dca174bbe2518d5fafc870213d70a58464d
thanhhd <thanhhd@aladintech.co>
2022-06-21 18:56:40 +0700

    test

--- split.py
+++ split.py
@@ -2,11 +2,13 @@ import cv2
 import os
 # 640*360 -> split 4x4 -> 160x90
 def split(path):
+    ROOT_DIR = os.path.abspath(os.curdir)
+    print(ROOT_DIR)
     img = cv2.imread(path)
     imgName = os.path.basename(path)
     imgName = imgName.split('.')[0]
     name = 1
-    dir = f'C:/Users/Admin/PycharmProjects/CSDLDPT/{imgName}'
+    dir = f'{ROOT_DIR}'
     isExist = os.path.exists(dir)
     if not isExist:
         os.makedirs(dir)
@@ -20,6 +22,6 @@ def split(path):
 
     for r in range(0,height,round(height/4)+1):
         for c in range(0,width,round(width/4)+1):
-            cv2.imwrite(os.path.join(dir, f'{name}.jpg'),img[r:r+round(height/4)+1, c:c+round(width/4)+1:])
+            cv2.imwrite(f'{dir}/{name}.jpg',img[r:r+round(height/4)+1, c:c+round(width/4)+1:])
             name += 1
     return
